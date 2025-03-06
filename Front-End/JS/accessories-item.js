4808d1a164a1d5c72375ea0d768a9b9ae5930311
LahiruButhsara34 <lahirubuthsarawijesekara@gmail.com>
2025-03-06 19:33:44 +0530

    Add product details and contact us pages ant its images

---
+++ Front-End/JS/accessories-item.js
@@ -0,0 +1,9 @@
+function addToCart() {
+  
+  let quantity = document.getElementById("quantity").value;
+  alert(`Added ${quantity} item(s)  to cart`);
+}
+
+function addToWishlist() {
+  alert("Added to Wishlist!");
+}
---
+++ Front-End/JS/main_item.js
@@ -0,0 +1,9 @@
+function addToCart() {
+    let size = document.getElementById("size").value;
+    let quantity = document.getElementById("quantity").value;
+    alert(`Added ${quantity} item(s) of size ${size} to cart`);
+}
+
+function addToWishlist() {
+    alert("Added to Wishlist!");
+}
