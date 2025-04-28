88968c3f46a6e33d585f7a0fde9706b4844236c4
Kalpani17 <sooriyasena2020@gmail.com>
2025-04-28 12:43:18 +0530

    Create product-db .sql

---
+++ product-db .sql
@@ -0,0 +1,82 @@
+INSERT INTO products1(Id,Name,Price,Category,Image,Stock,Product_detail,Status,Admin_id)
+VALUES
+('1','Printed Dress','3000','Dress','','10','Add a pop of color to your wardrobe with this stylish printed dress. Featuring a flattering silhouette, lightweight fabric, and vibrant patterns, it’s perfect for casual outings or special occasions. Comfortable, breathable, and effortlessly chic.','Available','1'),
+
+('2','Long Dress','3000','Dress','','10','Add a pop of color to your wardrobe with this stylish long dress. Featuring a flattering silhouette, lightweight fabric, and vibrant patterns, it’s perfect for casual outings or special occasions. Comfortable, breathable, and effortlessly chic.','Available','1'),
+
+('3','Short Dress','3000','Dress','','10','Add a pop of color to your wardrobe with this stylish short dress. Featuring a flattering silhouette, lightweight fabric, and vibrant patterns, it’s perfect for casual outings or special occasions. Comfortable, breathable, and effortlessly chic.','Available','1'),
+
+('4','Printed Dress','3000','Dress','','10','Add a pop of color to your wardrobe with this stylish printed dress. Featuring a flattering silhouette, lightweight fabric, and vibrant patterns, it’s perfect for casual outings or special occasions. Comfortable, breathable, and effortlessly chic.','Available','1'),
+
+('5','Cotton Trouser','3000','Trouser','','10','Stay comfortable and stylish with these classic cotton trousers. Made from soft, breathable fabric, they offer a relaxed fit and versatile design — perfect for everyday wear, work, or casual outings.','Available','1'),
+
+('6','Office Trouser','3000','Trouser','','10','Stay comfortable and stylish with these classic office trousers. Made from soft, breathable fabric, they offer a relaxed fit and versatile design — perfect for everyday wear, work, or casual outings.','Available','1'),
+
+('7','Cotton Trouser','3000','Trouser','','10','Stay comfortable and stylish with these classic cotton trousers. Made from soft, breathable fabric, they offer a relaxed fit and versatile design — perfect for everyday wear, work, or casual outings.','Available','1'),
+
+('8','Office Trouser','3000','Trouser','','10','Stay comfortable and stylish with these classic office trousers. Made from soft, breathable fabric, they offer a relaxed fit and versatile design — perfect for everyday wear, work, or casual outings.','Available','1'),
+
+('9','Printed Top','3000','Blouse','','10','Elevate your look with this chic printed blouse. Crafted from lightweight fabric, it features vibrant patterns and a flattering fit — perfect for both casual days and dressed-up evenings.','Available','1'),
+
+('10','Ladies Top','3000','Blouse','','10','Elevate your look with this chic ladies top. Crafted from lightweight fabric, it features vibrant patterns and a flattering fit — perfect for both casual days and dressed-up evenings.','Available','1'),
+
+('11','Printed Top','3000','Blouse','','10','Elevate your look with this chic lprinted blouse. Crafted from lightweight fabric, it features vibrant patterns and a flattering fit — perfect for both casual days and dressed-up evenings.','Available','1'),
+
+('12','Ladies Top','3000','Blouse','','10','Elevate your look with this chic ladies top. Crafted from lightweight fabric, it features vibrant patterns and a flattering fit — perfect for both casual days and dressed-up evenings.','Available','1'),
+
+('13','Mens Joggers','3000','Pants','','10','Versatile and stylish, these pants offer a perfect blend of comfort and sophistication. Designed with a flattering fit and high-quality fabric, they’re ideal for both workdays and weekends.','Available','1'),
+
+('14','Smart Casual','3000','Pants','','10','Versatile and stylish, these pants offer a perfect blend of comfort and sophistication. Designed with a flattering fit and high-quality fabric, they’re ideal for both workdays and weekends.','Available','1'),
+
+('15','Mens Joggers','3000','Pants','','10','Versatile and stylish, these pants offer a perfect blend of comfort and sophistication. Designed with a flattering fit and high-quality fabric, they’re ideal for both workdays and weekends.','Available','1'),
+
+('16','Smart Casual','3000','Pants','','10','Versatile and stylish, these pants offer a perfect blend of comfort and sophistication. Designed with a flattering fit and high-quality fabric, they’re ideal for both workdays and weekends.','Available','1'),
+
+('17','Casual Shirt','3000','Shirt','','10','A timeless essential, this shirt combines classic style with everyday comfort. Made from soft, breathable fabric, its perfect for both casual and formal looks.','Available','1'),
+
+('18','Denim Shirt','3000','Shirt','','10','A timeless essential, this shirt combines classic style with everyday comfort. Made from soft, breathable fabric, its perfect for both casual and formal looks.','Available','1'),
+
+('19','Casual Shirt','3000','Shirt','','10','A timeless essential, this shirt combines classic style with everyday comfort. Made from soft, breathable fabric, its perfect for both casual and formal looks.','Available','1'),
+
+('20','Denim Shirt','3000','Shirt','','10','A timeless essential, this shirt combines classic style with everyday comfort. Made from soft, breathable fabric, its perfect for both casual and formal looks.','Available','1'),
+
+('21','Neck T-Shirt','3000','T-Shirt','','10','Stay cool and casual with this classic t-shirt. Crafted from soft, breathable fabric, it offers a comfortable fit that is perfect for everyday wear.','Available','1'),
+
+('22','Neck T-Shirt','3000','T-Shirt','','10','Stay cool and casual with this classic t-shirt. Crafted from soft, breathable fabric, it offers a comfortable fit that is perfect for everyday wear.','Available','1'),
+
+('23','Neck T-Shirt','3000','T-Shirt','','10','Stay cool and casual with this classic t-shirt. Crafted from soft, breathable fabric, it offers a comfortable fit that is perfect for everyday wear.','Available','1'),
+
+('24','Neck T-Shirt','3000','T-Shirt','','10','Stay cool and casual with this classic t-shirt. Crafted from soft, breathable fabric, it offers a comfortable fit that is perfect for everyday wear.','Available','1'),
+
+('25','Short Dress','3000','Girls','','10','Brighten up her wardrobe with our adorable girls wear collection. Designed for comfort and style, each piece features playful patterns, soft fabrics, and a perfect fit for all-day fun.','Available','1'),
+
+('26','Part Wear','3000','Girls','','10','Brighten up her wardrobe with our adorable girls wear collection. Designed for comfort and style, each piece features playful patterns, soft fabrics, and a perfect fit for all-day fun.','Available','1'),
+
+('27','Short Dress','3000','Girls','','10','Brighten up her wardrobe with our adorable girls wear collection. Designed for comfort and style, each piece features playful patterns, soft fabrics, and a perfect fit for all-day fun','Available','1'),
+
+('28','Part Wear','3000','Girls','','10','Brighten up her wardrobe with our adorable girls wear collection. Designed for comfort and style, each piece features playful patterns, soft fabrics, and a perfect fit for all-day fun','Available','1'),
+
+('29','Printed Shirt','3000','Boys','','10','Keep him looking sharp and feeling comfortable with our boys wear collection. Featuring durable fabrics and cool, stylish designs, it is perfect for everyday adventures and special occasions.','Available','1'),
+
+('30','Graphic T-Shirt','3000','Boys','','10','Keep him looking sharp and feeling comfortable with our boys wear collection. Featuring durable fabrics and cool, stylish designs, it is perfect for everyday adventures and special occasions.','Available','1'),
+
+('31','Printed Shirt','3000','Boys','','10','Keep him looking sharp and feeling comfortable with our boys wear collection. Featuring durable fabrics and cool, stylish designs, it is perfect for everyday adventures and special occasions.','Available','1'),
+
+('32','Graphic T-Shirt','3000','Boys','','10','Keep him looking sharp and feeling comfortable with our boys wear collection. Featuring durable fabrics and cool, stylish designs, it is perfect for everyday adventures and special occasions.','Available','1'),
+
+('33','Citizen Ladies watch','3000','Ladies','','10','Elegant and timeless, this ladies watch combines style and functionality. Designed with a sleek finish and precise movement, it’s the perfect accessory for any occasion.','Available','1'),
+
+('34','Citizen Ladies watcht','3000','Ladies','','10','Elegant and timeless, this ladies watch combines style and functionality. Designed with a sleek finish and precise movement, it’s the perfect accessory for any occasion.','Available','1'),
+
+('35','Citizen Ladies watch','3000','Ladies','','10','Elegant and timeless, this ladies watch combines style and functionality. Designed with a sleek finish and precise movement, it’s the perfect accessory for any occasion','Available','1'),
+
+('36','Citizen Ladies watch','3000','Ladies','','10','Elegant and timeless, this ladies watch combines style and functionality. Designed with a sleek finish and precise movement, it’s the perfect accessory for any occasion','Available','1'),
+
+('37','Titan Mens Watch','3000','Mens','','10','Sophisticated and durable, this mens watch is crafted for style and performance. Featuring a sleek design and reliable timekeeping, it’s the perfect addition to any modern wardrobe.','Available','1'),
+
+('38','Titan Mens Watch','3000','Mens','','10','Sophisticated and durable, this mens watch is crafted for style and performance. Featuring a sleek design and reliable timekeeping, it’s the perfect addition to any modern wardrobe.','Available','1'),
+
+('39','Titan Mens Watch','3000','Mens','','10','Sophisticated and durable, this mens watch is crafted for style and performance. Featuring a sleek design and reliable timekeeping, it’s the perfect addition to any modern wardrobe.','Available','1'),
+
+('40','Titan Mens Watch','3000','Mens','','10','Sophisticated and durable, this mens watch is crafted for style and performance. Featuring a sleek design and reliable timekeeping, it’s the perfect addition to any modern wardrobe.','Available','1');
+
